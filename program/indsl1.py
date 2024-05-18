ed739106473e52b4a258ce14740badaa6996e5c4
AkselSukub <aksel.sukubov@yandex.ru>
2024-05-18 12:58:04 +0300

    finot

---
+++ program/indsl1.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import sys
+from datetime import date
+from jsonschema import validate, ValidationError
+
+
+def get_worker():
+    """
+    Запросить данные о рейсе.
+    """
+    point = input("Пункт назначения? ")
+    number = int(input("Номер рейса? "))
+    typex = input("Тип самолета? ")
+
+    # Создать словарь.
+    return {
+        "point": point,
+        "number": number,
+        "typex": typex,
+    }
+
+
+def display_workers(staff):
+    """
+    Отобразить список рейсов.
+    """
+    # Проверить, что список рейсов не пуст.
+    if staff:
+        # Заголовок таблицы.
+        line = "+-{}-+-{}-+-{}-+-{}-+".format(
+            "-" * 4, "-" * 30, "-" * 10, "-" * 20
+        )
+        print(line)
+        print(
+            "| {:^4} | {:^30} | {:^10} | {:^20} |".format(
+                "No", "Пункт назначения", "No рейса", "Тип самолета"
+            )
+        )
+        print(line)
+        # Вывести данные о всех рейсах.
+        for idx, worker in enumerate(staff, 1):
+            print(
+                "| {:>4} | {:<30} | {:<10} | {:>20} |".format(
+                    idx,
+                    worker.get("point", ""),
+                    worker.get("number", 0),
+                    worker.get("typex", ""),
+                )
+            )
+        print(line)
+    else:
+        print("Список рейсов пуст.")
+
+
+def select_workers(staff, period):
+    """
+    Выбрать работников с заданным стажем.
+    """
+    # Получить текущую дату.
+    today = date.today()
+    # Сформировать список рейсов.
+    result = []
+    for employee in staff:
+        if today.year - employee.get("year", today.year) >= period:
+            result.append(employee)
+    # Возвратить список выбранных рейсов.
+    return result
+
+
+def save_workers(file_name, staff):
+    """
+    Сохранить все рейсы в файл JSON.
+    """
+    # Открыть файл с заданным именем для записи.
+    with open(file_name, "w", encoding="utf-8") as fout:
+        # Выполнить сериализацию данных в формат JSON.
+        # Для поддержки кирилицы установим ensure_ascii=False
+        json.dump(staff, fout, ensure_ascii=False, indent=4)
+
+
+def load_workers(file_name):
+    """
+    Загрузить все рейсы из файла JSON.
+    """
+    schema = {
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "point": {"type": "string"},
+                "number": {"type": "integer"},
+                "typex": {"type": "string"},
+            },
+            "required": [
+                "point",
+                "number",
+                "typex",
+            ],
+        },
+    }
+    '''# Открыть файл с заданным именем для чтения.
+    with open(file_name, "r", encoding="utf-8") as fin:
+        return json.load(fin)'''
+        # Открыть файл с заданным именем для чтения.
+    with open(file_name, "r", encoding="utf-8") as fin:
+        data = json.load(fin)
+    
+    try:
+        # Валидация
+        validate(instance=data, schema=schema)
+        print("JSON валиден по схеме.")  # Выводим сообщение об успешной валидации
+    except ValidationError as e:
+        print(f"Ошибка валидации: {e.message}")
+    return data
+
+
+def main():
+    """
+    Главная функция программы.
+    """
+    # Список рейсов.
+    workers = []
+    # Организовать бесконечный цикл запроса команд.
+    while True:
+        # Запросить команду из терминала.
+        command = input(">>> ").lower()
+        # Выполнить действие в соответствие с командой.
+        if command == "exit":
+            break
+        elif command == "add":
+            # Запросить данные о рейсе.
+            worker = get_worker()
+            # Добавить словарь в список.
+            workers.append(worker)
+            # Отсортировать список в случае необходимости.
+            if len(workers) > 1:
+                workers.sort(key=lambda item: item.get("name", ""))
+        elif command == "list":
+            # Отобразить все рейсы.
+            display_workers(workers)
+        elif command.startswith("select "):
+            # Разбить команду на части для выделения стажа.
+            parts = command.split(maxsplit=1)
+            # Получить требуемый стаж.
+            period = int(parts[1])
+            # Выбрать рейсы с заданным стажем.
+            selected = select_workers(workers, period)
+            # Отобразить выбранные рейсы.
+            display_workers(selected)
+        elif command.startswith("save "):
+            # Разбить команду на части для выделения имени файла.
+            parts = command.split(maxsplit=1)
+            # Получить имя файла.
+            file_name = parts[1]
+            # Сохранить данные в файл с заданным именем.
+            save_workers(file_name, workers)
+        elif command.startswith("load "):
+            # Разбить команду на части для выделения имени файла.
+            parts = command.split(maxsplit=1)
+            # Получить имя файла.
+            file_name = parts[1]
+            # Сохранить данные в файл с заданным именем.
+            workers = load_workers(file_name)
+        elif command == "help":
+            # Вывесфввти справку о работе с программой.
+            print("Список команд:\n")
+            print("add - добавить работника;")
+            print("list - вывести список работников;")
+            print("select <стаж> - запросить работников со стажем;")
+            print("help - отобразить справку;")
+            print("load - загрузить данные из файла;")
+            print("save - сохранить данные в файл;")
+            print("exit - завершить работу с программой.")
+        else:
+            print(f"Неизвестная команда {command}", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    main()
